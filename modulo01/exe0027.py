13d5405b4eea36be133587abdb200ff8676748f8
Gabriel Costa Lucas <gcostaluccas95@gmail.com>
2023-08-09 15:58:57 -0300

    up

--- modulo01/exe0027.py
+++ modulo01/exe0027.py
@@ -1,6 +1,6 @@
 # receba um nome e retorne somente sue primeiro e ultimo nome
 
-nome = input("Digite seu nome ")
+nome = str(input("Digite seu nome "))
 split = nome.split()
 nome1 = split[0]
 nome2 = split[-1]
--- modulo01/exe025.py
+++ modulo01/exe025.py
@@ -1,7 +1,7 @@
 #receba um nome e me diga sim ou não se começa com SANTO
 
 name=input("Diga me o nome de sua cidade ")
-snmae=name.split()
+snmae= name.strip().split()
 sim="Santos" in snmae[0]
 
 print(f"sua cidade {sim} tem Santos no começo do nome")
